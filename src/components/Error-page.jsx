98d466251bfc833a9b3631a0345d24828f213651
BobbieV <bobbiev@hawaii.edu>
2024-01-03 08:44:23 -1000

    create Error-page

---
+++ src/components/Error-page.jsx
@@ -0,0 +1,11 @@
+
+
+export default function ErrorPage() {
+
+    return (
+        <div id="error-page">
+            <h1 className="oops">Oops!</h1>
+            <p className="sorryText">Sorry, an unexpected error has occurred.</p>
+        </div>
+    )
+}
