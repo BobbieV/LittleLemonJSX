6645a6652d9d83b675c867f8201fade6163f3ad2
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-01-04 20:15:49 -1000

    add import bookingData to Bookings.jsx

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { BookingProvider, useBookingContext} from "./BookingContext.jsx";
+import { BookingProvider, useBookingContext, bookingData} from "./BookingContext.jsx";
 
 
 
