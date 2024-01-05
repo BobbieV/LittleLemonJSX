0d51ddb62442e92cab2e3d541cbc2f9d4400bb13
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-01-04 20:10:26 -1000

    test e.target.value instead of "

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -36,7 +36,7 @@ const BookingForm = () => {
         e.preventDefault();
         console.log("Form Submitted!");
         alert("Your reservation is Confirmed")
-        setResName("");
+        setResName(e.target.value);
         setDate("");
         setTime("");
         setNumGuests("");
