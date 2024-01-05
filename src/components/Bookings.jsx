88d39e8e7654c17f47ab23b99bc1817b41397b3e
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-01-05 07:31:54 -1000

    Add <p> details

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -24,11 +24,11 @@ function Bookings() {
                 <h2 className="availH2">Your </h2>
                 <h2 className="availH2">Reservation</h2>
             </header>
-            <p>{resName}</p>
-            <p>{date}</p>
-            <p>{time}</p>
-            <p>{numGuests}</p>
-            <p>{occasion}</p>
+            <p>Your Name: {resName}</p>
+            <p>Date of Reservation: {date}</p>
+            <p>Time of Reservation: {time}</p>
+            <p>Number of Guests: {numGuests}</p>
+            <p>Occasion: {occasion}</p>
         <Link to="/components/BookingsPage">Edit Reservation</Link>
         <Link to="/">Home</Link>
 
