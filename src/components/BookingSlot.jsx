10b2a04c794c218320cbe96777b4c21d77b8a245
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-01-07 08:38:34 -1000

    remove time props from const BookingSlot

--- src/components/BookingSlot.jsx
+++ src/components/BookingSlot.jsx
@@ -1,7 +1,7 @@
 import { useBookingContext } from './BookingContext';
 
 
-const BookingSlot = ({ time }) => {
+const BookingSlot = () => {
     const { availability, available, time } = useBookingContext();
     const handleClick = (selectedTime) => {
         const selectedSlot = availability.find(slot => slot.time === selectedTime);
