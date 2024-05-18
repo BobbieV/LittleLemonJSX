87dcc3adf44278e16476b5948fc98e8fedb7f658
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-05-18 10:31:19 -1000

    Resolved availability/availableTimes inconsistencies.  Maybe.

--- src/components/BookingSlot.jsx
+++ src/components/BookingSlot.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-
+// Renders an option element for time slots within BookingForm.jsx
 const BookingSlot = ({ time, available }) => {
     return (
-            <option value={time} disabled={!available}>
+        // Display the available time, but disable if unavailble
+            <option value={time} disabled={!available}> 
                 {time} {available ? '(Available)' : '(Not Available)'}
             </option>
     )
