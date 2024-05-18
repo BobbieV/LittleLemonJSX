0e67d5c6e683b7eaad42f1b8c8e3a345e7a92916
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-05-17 15:54:08 -1000

    attempt consitency with selectedTime variable

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -18,11 +18,11 @@ function Bookings() {
                         <h2 className="availH2">Your </h2>
                         <h2 className="availH2">Reservation</h2>
                     </header>
-                    <p>Your Name: {resName}</p>
-                    <p>Date of Reservation: {date}</p>
-                    <p>Time of Reservation: {time}</p>
-                    <p>Number of Guests: {numGuests}</p>
-                    <p>Occasion: {occasion}</p>
+                    <p>Your Name: { resName }</p>
+                    <p>Date of Reservation: { date }</p>
+                    <p>Time of Reservation: { selectedTime }</p>
+                    <p>Number of Guests: { numGuests }</p>
+                    <p>Occasion: { occasion }</p>
                     <Link to="/components/BookingsPage">Edit Reservation</Link>
                     <Link to="/">Home</Link>
 
