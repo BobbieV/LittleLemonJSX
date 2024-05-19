8627e3186c99e309065c7edcf337017f87f2c859
Bobbie VanBatenburg <122819646+BobbieV@users.noreply.github.com>
2024-05-18 16:55:59 -1000

    reposition provider

--- src/App.jsx
+++ src/App.jsx
@@ -18,81 +18,82 @@ import Bookings from './components/Bookings.jsx';
 function App() {
 
   return (
-    <CombinedProvider>
       <BrowserRouter >
-        <div className="navLinksWrapper">
-          <nav id="nav">
-            <img
-              src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/logo.png?raw=true"
-              className="AppLogo"
-              alt="A lemon icon followed by the words 'Little Lemon'">
-            </img>
+        <CombinedProvider>
+          <div className="navLinksWrapper">
+            <nav id="nav">
+              <img
+                src="https://github.com/BobbieV/littleLemon/blob/main/src/assets/images/logo.png?raw=true"
+                className="AppLogo"
+                alt="A lemon icon followed by the words 'Little Lemon'">
+              </img>
 
-            <NavLink
-              to="/"
-              className="navbar navtextHome"
-              style={({ isActive }) => ({
-                color: isActive ? '#495E57' : '#333333'
-              })}
-              >Home
-            </NavLink>
-            <NavLink
-              to="/about"
-              className="navbar navtextAbout"
-              style={({ isActive }) => ({
-                color: isActive ? '#495E57' : '#333333'
-              })}
-              >About
-            </NavLink>
-            <NavLink
-              to="/specials"
-              className="navbar navtextMenu"
-              style={({ isActive }) => ({
-                color: isActive ? '#495E57' : '#333333'
-              })}
-              >Menu
-            </NavLink>
-            <NavLink
-              to="/components/BookingsPage"
-              className="navbar navtextReservations"
-              style={({ isActive }) => ({
-                color: isActive ? '#495E57' : '#333333'
-              })}
-              >Reservations
-            </NavLink>
-            <NavLink
-              to="/orderOnline"
-              className="navbar navtextOrderonline"
-              style={({ isActive }) => ({
-                color: isActive ? '#495E57' : '#333333'
-              })}
-              >Order Online
-            </NavLink>
-            <NavLink
-              to="/login"
-              className="navbar navtextLogin"
-              style={({ isActive }) => ({
-                color: isActive ? '#495E57' : '#333333'
-              })}
-              >Login
-            </NavLink>
+              <NavLink
+                to="/"
+                className="navbar navtextHome"
+                style={({ isActive }) => ({
+                  color: isActive ? '#495E57' : '#333333'
+                })}
+                >Home
+              </NavLink>
+              <NavLink
+                to="/about"
+                className="navbar navtextAbout"
+                style={({ isActive }) => ({
+                  color: isActive ? '#495E57' : '#333333'
+                })}
+                >About
+              </NavLink>
+              <NavLink
+                to="/specials"
+                className="navbar navtextMenu"
+                style={({ isActive }) => ({
+                  color: isActive ? '#495E57' : '#333333'
+                })}
+                >Menu
+              </NavLink>
+              <NavLink
+                to="/components/BookingsPage"
+                className="navbar navtextReservations"
+                style={({ isActive }) => ({
+                  color: isActive ? '#495E57' : '#333333'
+                })}
+                >Reservations
+              </NavLink>
+              <NavLink
+                to="/orderOnline"
+                className="navbar navtextOrderonline"
+                style={({ isActive }) => ({
+                 color: isActive ? '#495E57' : '#333333'
+                })}
+                >Order Online
+              </NavLink>
+              <NavLink
+                to="/login"
+                className="navbar navtextLogin"
+                style={({ isActive }) => ({
+                  color: isActive ? '#495E57' : '#333333'
+                })}
+                >Login
+              </NavLink>
 
-          </nav>
-        </div>
+            </nav>
+          </div>
 
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/components/BookingsPage" element={<BookingsPage />}/>
-          <Route path="/Specials" element={<Specials />}/>
-          <Route path="/About" element={<About />}/>
-          <Route path="/OrderOnline" element={<ErrorPage />}/>
-          <Route path="/Login" element={<ErrorPage />}/>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/components/BookingsPage" element={<BookingsPage />}/>
+            <Route path="/Specials" element={<Specials />}/>
+            <Route path="/About" element={<About />}/>
+            <Route path="/OrderOnline" element={<ErrorPage />}/>
+            <Route path="/Login" element={<ErrorPage />}/>
 
-          <Route path="/ContactForm" element={<ContactForm />}/>
-          <Route path="/components/Bookings" element={<Bookings />}/>
-        </Routes>
+            <Route path="/ContactForm" element={<ContactForm />}/>
+           <Route path="/components/Bookings" element={<Bookings />}/>
+          </Routes>
+        </CombinedProvider>
       </BrowserRouter>
-    </CombinedProvider>
+
   )
 }
 
